f6a260fa620e767c3444e9c5e08095f8f9fd5315
Victor Chepkasov <92711919+VictorChepkasov@users.noreply.github.com>
2023-07-07 15:18:27 +0500

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,1 +1,13 @@
-from brownie import IssueCertificate
+from brownie import certificateFactory, accounts
+
+def main():
+    account = accounts[0]
+    deployFactory(account)
+
+def deployFactory(_from):
+    factory = certificateFactory.deploy({
+        'from': _from,
+        'priority_fee': '1 wei'
+    })
+    print(f'Factory deployed at {factory}!')
+    return factory
