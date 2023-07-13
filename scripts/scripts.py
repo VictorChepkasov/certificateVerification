c1c147217c0d226228909ecd5ac2d3875f5da933
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-07-13 09:44:30 +0500

    write scripts

---
+++ scripts/scripts.py
@@ -0,0 +1,19 @@
+from brownie import certificateFactory, accounts
+from scripts.deploy import deployFactory as deploy
+
+def issueCertificate(_from, _name, _validity):
+    certificateFactory[-1].issueCertificate(
+        _name, _validity, {
+            'from': _from,
+            'priority_fee': '1 wei'
+        }
+    )
+    print('Certificate issued successfully!')
+
+def revokeCertificate(certificate):
+    certificateFactory[-1].revokeCertificate(certificate)
+    print('Certificate revorked!')
+
+def verifyCertificate(certificate):
+    certificateFactory[-1].verifyCertificate(certificate)
+    print('Certificate verified!')
