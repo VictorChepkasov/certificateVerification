4b1f4f36f5a7228201a81d9736d4504a8b72f30d
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-07-14 23:31:56 +0500

    edit & parametrize tests

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -6,30 +6,29 @@ from scripts.scripts import issueCertificate, revokeCertificate, verifyCertifica
 from web3 import Web3, EthereumTesterProvider
 w3 = Web3(EthereumTesterProvider())
 
+pytestmark = pytest.mark.parametrize('_validity', [0, 1, 2])
+
 @pytest.fixture()
 def deployedCertificate(autouse=True):
     acc = accounts[0]
     certificateContract = deploy(acc)
     return acc, certificateContract
 
-def test_issueCertificate(deployedCertificate, _name='First', _validity=0):
+def test_issueCertificate(deployedCertificate, _validity, _name='First'):
     acc, factoryContract = deployedCertificate
     issueCertificate(acc, _name, _validity)
-    timestamp = chain.time()+(86400*_validity)
+    timestamp = chain.time()
     certificateInfo = getCertificateInfo(1)
     #допустим, что хэш всегда генерируется правильно
     documentHash = certificateInfo[2]
-    validInfo = (acc, _name, documentHash, 1, 1, chain.time(), timestamp, False, True)
+    validInfo = (acc, _name, documentHash, 1, 1, timestamp, timestamp+(86400*_validity), False, True)
     assert certificateInfo == validInfo
-    print(f'Factory: {factoryContract}')
     assert factoryContract.getTotalCertificates() == certificateInfo[3]
 
-def test_revokeCertificate(deployedCertificate, _name='Second', _validity=0):
+def test_revokeCertificate(deployedCertificate, _validity, _name='Second'):
     acc, factoryContract = deployedCertificate
     issueCertificate(acc, _name, _validity)
     totalCertificates = factoryContract.getTotalCertificates()
-    certificate = factoryContract.getCertificate(1)
-    print(f'Certificate: {certificate}')
     revokeCertificate(acc, 1)
     revoked = getCertificateInfo(1)[-2]
     assert factoryContract.getRevorkedCertificates() == 1
@@ -37,12 +36,9 @@ def test_revokeCertificate(deployedCertificate, _name='Second', _validity=0):
     # What the fuck?
     assert revoked == True
 
-def test_verifyCertificate(deployedCertificate, _id=1, _name='Third', _validity=0):
+def test_verifyCertificate(deployedCertificate, _validity, _id=1, _name='Third',):
     acc, factoryContract = deployedCertificate
     issueCertificate(acc, _name, _validity)
-
     certificate = factoryContract.getCertificate(_id)
     verifyInfo = verifyCertificate(acc, certificate, _id).return_value
-    print(f'Verify info: {verifyInfo}')
-
     assert verifyInfo == getCertificateInfo(_id)
